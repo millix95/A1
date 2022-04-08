70dd5fcc76f41079dfc3dc7e8ffd64b10b669b6b
Milos Dobrijevic <dobry95@gmail.com>
2022-04-08 23:22:13 +0200

    A1 - dodata sekcija 'Trebaju ti dodaci?'

--- script.js
+++ script.js
@@ -50,3 +50,43 @@ function prikazati4() {
     karticaJedan.style.display = 'none';
     karticaTri.style.display = 'grid';
 }
+
+// SEKCIJA KRETANJA SLIKE
+
+let strelicaD = document.querySelector('#dve-slike-strelica-desno');
+let strelicaL = document.querySelector('#dve-slike-strelica-levo');
+let sekcijaL = document.querySelector('#dve-slike');
+let sekcijaD = document.querySelector('#dve-slike2');
+let animacijaStreliceL = document.querySelector('#dve-slike-strelica-levo-slika');
+let animacijaStreliceD = document.querySelector('#dve-slike-strelica-desno-slika');
+
+
+strelicaD.addEventListener('click', prebaciSekciju);
+function prebaciSekciju(e) {
+    e.preventDefault();
+    sekcijaL.style.display = 'none';
+    sekcijaL.style.animationName = 'fadeOut';
+    sekcijaL.style.animationDuration = '2s';
+    sekcijaD.style.display = 'grid';
+    sekcijaD.style.animationName = 'fadeIn';
+    sekcijaD.style.animationDuration = '2s';
+    animacijaStreliceD.style.display = 'none';
+    animacijaStreliceL.style.display = 'grid';
+    strelicaL.style.display = 'grid';
+}
+
+strelicaL.addEventListener('click', prebaciSekciju2);
+function prebaciSekciju2(e2) {
+    e2.preventDefault();
+    sekcijaL.style.display = 'grid';
+    sekcijaL.style.animationName = 'fadeIn';
+    sekcijaL.style.animationDuration = '2s';
+    sekcijaD.style.display = 'none';
+    sekcijaD.style.animationName = 'fadeOut';
+    sekcijaD.style.animationDuration = '2s';
+    animacijaStreliceD.style.display = 'grid';
+    animacijaStreliceL.style.display = 'none';
+    strelicaL.style.display = 'none';
+    strelicaD.style.display = 'grid';
+
+}
